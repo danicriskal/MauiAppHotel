3b99723cf9a8111e8aa4330858d72e5c4b27de74
danicriskal <danicriskal@gmail.com>
2025-05-22 18:59:34 -0300

    Add recursos ao aplicativo

--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,6 +13,9 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("DMSerifText-Italic.ttf", "DM-Italic");
+                    fonts.AddFont("DMSerifText-Regular.ttf", "DM");
+
                 });
 
 #if DEBUG
